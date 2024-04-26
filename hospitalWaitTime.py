0f279dba2ffac57978c5d1bf73bf8c3aced18548
Maham Farooq <67932027+CoderMF@users.noreply.github.com>
2024-04-26 11:43:57 -0400

    modularized and working through SMH

--- hospitalWaitTime.py
+++ hospitalWaitTime.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 # URL of the JSON file
 GRH_json_url = 'https://www.grhosp.on.ca/assets/documents/current-emergency-waits.json'
-SMH_url = 'https://www.smgh.ca/patients-visitors/emergency-department-wait-times'
+SMH_url = 'https://smgh-predict.oculys.com/'
 
 def json_scraper(url,arrival,patients,date):
     # Send a GET request to the URL
@@ -30,7 +30,7 @@ def scraper (url,arrival,patients,date):
         soup = BeautifulSoup(response.text, 'html.parser')
         
         # Find the element containing the wait time using the ID
-        wait_time = soup.find('div', class_=patients)
+        wait_time = soup.find('div', id=arrival)
     
         if wait_time:
             print("Current Wait Time:", wait_time.text.strip())
@@ -44,5 +44,5 @@ def texts(arrival,patients,date):
         print("Patients Waiting:", patients)
         print("Data Last Updated On:", date)
 
-#scraper (GRH_json_url,'arrival-to-md','patients-waiting','date-created')
-scraper (SMH_url,'absoluteHorCenter stdStopwatchMiddleText','stdPatientsWaitingText','absoluteHorCenter stdStopwatchBottomText')
+#json_scraper (GRH_json_url,'arrival-to-md','patients-waiting','date-created')
+scraper (SMH_url,'mainStopwatch','currentPatients','stdStopwatchBottomText')
